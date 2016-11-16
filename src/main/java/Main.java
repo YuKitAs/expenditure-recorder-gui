b67e8b080ddc237740a4c1fd1a3cff1692219d4b
YuKitAs <moncher.merci@gmail.com>
2016-11-16 21:03:36 +0100

    Changed some colors and fonts.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +12,9 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("expenditure-recorder-gui.fxml"));
         primaryStage.setTitle("Expenditure Recorder");
         Scene scene = new Scene(root, 501, 500);
-        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+        Font.loadFont(getClass().getResourceAsStream("KGTraditionalFractions.ttf"), 30);
+        Font.loadFont(getClass().getResourceAsStream("Roundo-SemiBold.otf"), 15);
+        scene.getStylesheets().addAll(getClass().getResource("style.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
     }
