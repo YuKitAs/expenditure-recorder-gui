b2bab9801865126bad368044ba5368863013d64f
YuKitAs <moncher.merci@gmail.com>
2017-01-29 14:00:42 +0100

    Unit tests for model

--- src/test/java/expenditure/recorder/gui/viewmodel/RecordTableItemTest.java
+++ src/test/java/expenditure/recorder/gui/viewmodel/RecordTableItemTest.java
@@ -1,5 +1,38 @@
 package expenditure.recorder.gui.viewmodel;
 
-public class RecordTableItemTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class RecordTableItemTest {
+    RecordTableItem recordTableItem;
+
+    @Before
+    public void setUp() {
+        recordTableItem = RecordTableItem.from("some item", "42", LocalDate.now());
+    }
+
+    @Test
+    public void getItemWithCorrectItemString() {
+        assertThat(recordTableItem.getItem()).isEqualTo("some item");
+    }
+
+    @Test
+    public void getAmountWithCorrectAmountString() {
+        assertThat(recordTableItem.getAmount()).isEqualTo("42.00");
+    }
+
+    @Test
+    public void getAmountInCentWithCorrectAmountInteger() {
+        assertThat(recordTableItem.getAmountInCent()).isEqualTo(4200);
+    }
+
+    @Test
+    public void getDateWithCorrectDate() {
+        assertThat(recordTableItem.getDate()).isEqualTo(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    }
 }
--- src/test/java/expenditure/recorder/gui/viewmodel/utilities/MoneyFormatterTest.java
+++ src/test/java/expenditure/recorder/gui/viewmodel/utilities/MoneyFormatterTest.java
@@ -1,5 +1,14 @@
 package expenditure.recorder.gui.viewmodel.utilities;
 
-public class MoneyFormatterTest {
+import org.junit.Test;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class MoneyFormatterTest {
+    @Test
+    public void formatWithCorrectString() {
+        assertThat(MoneyFormatter.format(100)).isEqualTo("1.00");
+        assertThat(MoneyFormatter.format(10)).isEqualTo("0.10");
+        assertThat(MoneyFormatter.format(0)).isEqualTo("0.00");
+    }
 }
