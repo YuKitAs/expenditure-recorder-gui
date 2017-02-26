2af509ff93933b46158a759f2bd1464ad7e0f1ea
YuKitAs <moncher.merci@gmail.com>
2017-02-26 11:59:09 +0100

    Convert amount String to amountInCent.

--- src/main/java/expenditure/recorder/gui/viewmodel/utilities/MoneyFormatter.java
+++ src/main/java/expenditure/recorder/gui/viewmodel/utilities/MoneyFormatter.java
@@ -4,9 +4,13 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class MoneyFormatter {
-    public static String format(Integer amountInCent) {
+    public static String formatIntegerToString(Integer amountInCent) {
         DecimalFormat df = new DecimalFormat("0.00");
         df.setRoundingMode(RoundingMode.HALF_UP);
         return df.format(amountInCent / 100.0);
+    }
+
+    public static Integer formatStringToInteger(String amount) {
+        return (int) (Double.parseDouble(amount) * 100);
     }
 }
--- src/test/java/expenditure/recorder/gui/viewmodel/utilities/MoneyFormatterTest.java
+++ src/test/java/expenditure/recorder/gui/viewmodel/utilities/MoneyFormatterTest.java
@@ -7,8 +7,8 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class MoneyFormatterTest {
     @Test
     public void formatWithCorrectString() {
-        assertThat(MoneyFormatter.format(100)).isEqualTo("1.00");
-        assertThat(MoneyFormatter.format(10)).isEqualTo("0.10");
-        assertThat(MoneyFormatter.format(0)).isEqualTo("0.00");
+        assertThat(MoneyFormatter.formatIntegerToString(100)).isEqualTo("1.00");
+        assertThat(MoneyFormatter.formatIntegerToString(10)).isEqualTo("0.10");
+        assertThat(MoneyFormatter.formatIntegerToString(0)).isEqualTo("0.00");
     }
 }
