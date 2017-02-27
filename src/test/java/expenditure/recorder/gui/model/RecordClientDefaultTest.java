0523aa16688748f6a88e14b42e5f64301ba30298
YuKitAs <moncher.merci@gmail.com>
2017-02-27 23:37:33 +0100

    Configured a fake server und tested get request in RecordClient.

--- src/test/java/expenditure/recorder/gui/model/RecordClientDefaultTest.java
+++ src/test/java/expenditure/recorder/gui/model/RecordClientDefaultTest.java
@@ -1,19 +1,34 @@
 package expenditure.recorder.gui.model;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
+import java.time.Instant;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RecordClientDefaultTest {
     private final RecordClientDefault recordClient = new RecordClientDefault();
+    @Rule
+    public WireMockRule wm = new WireMockRule(options().port(8080));
 
     @Test
     public void getAllRecordsFromServer_WithCorrectRecords() throws IOException {
-        List<Record> records = recordClient.getAllRecordsFromServer();
+        stubFor(get(urlEqualTo("/expenditure_recorder/expenditures")).willReturn(aResponse().
+                withBody(
+                        "[{\"id\":\"00000000-0000-0000-0000-000000000000\",\"date\":1488233591,\"amountInCent\":100,\"item\":\"Hoho\"}]")));
 
-        assertThat(records).extracting(Record::getAmountInCent).doesNotContainNull();
+        assertThat(recordClient.getAllRecordsFromServer()).hasSize(1);
+        assertThat(recordClient.getAllRecordsFromServer().get(0).getDate()).isEqualTo(Instant.ofEpochMilli(1488233591));
+        assertThat(recordClient.getAllRecordsFromServer().get(0).getAmountInCent()).isEqualTo(100);
+        assertThat(recordClient.getAllRecordsFromServer().get(0).getItem()).isEqualTo("Hoho");
     }
 }
