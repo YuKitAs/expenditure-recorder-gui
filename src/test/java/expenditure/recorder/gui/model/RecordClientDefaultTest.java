bf27dbff8a7821d7175bb2e46e01ceb65ba5a01e
YuKitAs <moncher.merci@gmail.com>
2017-02-28 19:11:17 +0100

    Added a json file as request body and tested addRecordToServer method.

--- src/test/java/expenditure/recorder/gui/model/RecordClientDefaultTest.java
+++ src/test/java/expenditure/recorder/gui/model/RecordClientDefaultTest.java
@@ -9,9 +9,12 @@ import java.io.IOException;
 import java.time.Instant;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,5 +37,14 @@ public class RecordClientDefaultTest {
         assertThat(recordClient.getAllRecordsFromServer().get(1).getDate()).isEqualTo(Instant.ofEpochMilli(1475798400));
         assertThat(recordClient.getAllRecordsFromServer().get(1).getAmountInCent()).isEqualTo(600);
         assertThat(recordClient.getAllRecordsFromServer().get(1).getItem()).isEqualTo("Big Ice Cream");
+    }
+
+    @Test
+    public void addRecordToServer() throws IOException {
+        stubFor(post(urlPathEqualTo("/expenditure_recorder/expenditures")).withRequestBody(
+                equalToJson(ResourceFileReader.read(RecordClientDefaultTest.class, "NewExpenditureRecord.json"))));
+
+        Record record = new Record("00000000-0000-0000-0000-000000000000", "Huge Ice Cream", 1000, Instant.ofEpochMilli(1488240000));
+        recordClient.addRecordToServer(record);
     }
 }
