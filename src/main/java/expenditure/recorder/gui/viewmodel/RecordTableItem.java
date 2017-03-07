90028050bb76b38c22a8528ee0892a1688e05762
YuKitAs <moncher.merci@gmail.com>
2017-03-07 17:08:20 +0100

    Added id field.

--- src/main/java/expenditure/recorder/gui/viewmodel/RecordTableItem.java
+++ src/main/java/expenditure/recorder/gui/viewmodel/RecordTableItem.java
@@ -9,23 +9,29 @@ import expenditure.recorder.gui.model.Record;
 import expenditure.recorder.gui.viewmodel.utilities.MoneyFormatter;
 
 public class RecordTableItem {
+    private String id = null;
     private final String item;
     private final Integer amountInCent;
     private final Instant date;
 
-    private RecordTableItem(String item, Integer amountInCent, Instant date) {
+    private RecordTableItem(String id, String item, Integer amountInCent, Instant date) {
+        this.id = id;
         this.item = item;
         this.amountInCent = amountInCent;
         this.date = date;
     }
 
     public static RecordTableItem from(Record record) {
-        return new RecordTableItem(record.getItem(), record.getAmountInCent(), record.getDate());
+        return new RecordTableItem(record.getId(), record.getItem(), record.getAmountInCent(), record.getDate());
     }
 
     public static RecordTableItem from(String item, String amount, LocalDate date) {
-        return new RecordTableItem(item, MoneyFormatter.formatStringToInteger(amount),
+        return new RecordTableItem(null, item, MoneyFormatter.formatStringToInteger(amount),
                 date.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getItem() {
